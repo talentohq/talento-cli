15c30514b7f6178e599443eb2398d8432607794a
Jorge Alvarez <it@talentohq.com>
2026-09-02 18:17:02 +0200

    chore: stamp post-release 1.0.2-dev
    Keep master from claiming it is 1.0.1 now that v1.0.1 is published.

--- nix/version.nix
+++ nix/version.nix
@@ -1,4 +1,4 @@
 # Development checkouts deliberately report a development version. Before a
 # release tag is created, run scripts/stamp-nix-version.sh VERSION and commit
 # this one-line stamp. The release workflow refuses a tag that disagrees.
-"1.0.1"
+"1.0.2-dev"
